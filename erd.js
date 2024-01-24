36d0714a1829cfe1fd9f722ea9d3c222f2a1705a
Ali Bayat <alibyt21@gmail.com>
2024-01-24 11:19:26 +0330

    before version2

--- erd.js
+++ erd.js
@@ -2,7 +2,6 @@
 
 (function () {
 
-
     var graph = new joint.dia.Graph;
     var paper = new joint.dia.Paper({
         el: document.getElementById('myDiagram'),
@@ -74,7 +73,7 @@
                 }
             });
             rect.addTo(graph);
-            rect.toBack();
+            // rect.toBack();
             return rect;
         } else if (type == 'standard.Ellipse') {
             var rect = new joint.shapes.standard.Ellipse();
@@ -138,7 +137,6 @@
 
 
 
-
     var graphLayout = new joint.layout.ForceDirected({
         graph: graph,
         width: 1500,
@@ -172,5 +170,45 @@
     }
 
     $('#btn-layout').on('click', startAnimate);
-    
+
+
+
+    // joint.shapes.basic.DecoratedRect = joint.shapes.basic.Generic.extend({
+
+    //     markup: '<g class="rotatable"><g class="scalable"><rect/></g><image/><text/></g>',
+
+    //     defaults: joint.util.deepSupplement({
+
+    //         type: 'basic.DecoratedRect',
+    //         size: { width: 100, height: 60 },
+    //         attrs: {
+    //             'rect': { fill: '#FFFFFF', stroke: 'black', width: 100, height: 60 },
+    //             'text': { 'font-size': 14, text: '', 'ref-x': .5, 'ref-y': .5, ref: 'rect', 'y-alignment': 'middle', 'x-alignment': 'middle', fill: 'black' },
+    //             'text2': { 'font-size': 24, text: 'some text', 'ref-x': .7, 'ref-y': .7, ref: 'rect', 'y-alignment': 'middle', 'x-alignment': 'middle', fill: 'black' },
+
+    //             'image': { 'ref-x': 2, 'ref-y': 2, ref: 'rect', width: 16, height: 16 }
+    //         }
+
+    //     }, joint.shapes.basic.Generic.prototype.defaults)
+    // });
+
+    // var decoratedRect = new joint.shapes.basic.DecoratedRect({
+    //     position: { x: 150, y: 80 },
+    //     size: { width: 100, height: 60 },
+    //     attrs: { 
+    //         text: { text: 'My Element' },
+    //         text2: { text: 'hello world' },
+
+    //         image: { 'xlink:href': 'https://assets-global.website-files.com/63061d4ee85b5a18644f221c/64db4e708003b5f97676b7b1_react.png' }
+    //     }
+    // });
+    // graph.addCell(decoratedRect);
+
+
+
+
+
+
+
+
 })();
