1f3aec4c0eaebe58a37f5b5b6143c9a9e909d28b
Ali Bayat <alibyt21@gmail.com>
2024-01-27 13:45:17 +0330

    dataFlow added

---
+++ dataFlow.js
@@ -0,0 +1,446 @@
+'use strict';
+
+(function () {
+
+
+    let data = [
+        {
+            source_entity_title: "داده پایه کسب و کار (مرجع)",
+            dest_entity_title: "داده پایه کسب و کار",
+            source_entity_code: "401",
+            dest_entity_code: "308",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "مدارک تحصیلی مرجع - دانشگاه آزاد",
+            dest_entity_title: "مدارک تحصیلی",
+            source_entity_code: "3052",
+            dest_entity_code: "3001",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "اطلاعات ترمی دانشجو مرجع",
+            dest_entity_title: "اطلاعات ترمی دانشجو",
+            source_entity_code: "1001",
+            dest_entity_code: "621",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "رشته های تحصیلی دانشگاهی مرجع",
+            dest_entity_title: "رشته های تحصیلی دانشگاهی",
+            source_entity_code: "613",
+            dest_entity_code: "609",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "مدارک تحصیلی مرجع _ حوزه علمیه خواهران",
+            dest_entity_title: "مدارک تحصیلی",
+            source_entity_code: "3055",
+            dest_entity_code: "3001",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "اطلاعات نهایی تماس فرد",
+            dest_entity_title: "اطلاعات نهایی تماس فرد مخزن",
+            source_entity_code: "2005",
+            dest_entity_code: "110",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "دانشگاه ها",
+            dest_entity_title: "ویوی رشته دانشگاه های بین الملل",
+            source_entity_code: "608",
+            dest_entity_code: "616",
+            lineage_type: "mdm_2_destination"
+        },
+        {
+            source_entity_title: "دانشگاه های خارجی (مرجع)",
+            dest_entity_title: "دانشگاه های خارجی جدید",
+            source_entity_code: "599",
+            dest_entity_code: "601",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "نوع پذیرش",
+            dest_entity_title: "زیر داده پایه کسب و کار",
+            source_entity_code: "603",
+            dest_entity_code: "307",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "مدارک تحصیلی مرجع _ حوزه علمیه خواهران [Duplicate]",
+            dest_entity_title: "مدارک تحصیلی",
+            source_entity_code: "3069",
+            dest_entity_code: "3001",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "فرد",
+            dest_entity_title: "فرد مخزن",
+            source_entity_code: "2001",
+            dest_entity_code: "103",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "زیر داده پایه کسب و کار (مرجع) دو",
+            dest_entity_title: "زیر داده پایه کسب و کار",
+            source_entity_code: "3066",
+            dest_entity_code: "307",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "مدارک تحصیلی مرجع - وزارت بهداشت",
+            dest_entity_title: "مدارک تحصیلی",
+            source_entity_code: "3053",
+            dest_entity_code: "3001",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "اطلاعات کلی فارغ التحصیل مرجع",
+            dest_entity_title: "اطلاعات کلی فارغ التحصیل تستی",
+            source_entity_code: "618",
+            dest_entity_code: "617",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "مدارک تحصیلی",
+            dest_entity_title: "مدارک تحصیلی تست",
+            source_entity_code: "3001",
+            dest_entity_code: "3062",
+            lineage_type: "mdm_2_destination"
+        },
+        {
+            source_entity_title: "مدارک تحصیلی مرجع _ جامعه الزهرا",
+            dest_entity_title: "مدارک تحصیلی",
+            source_entity_code: "3056",
+            dest_entity_code: "3001",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "رشته های تحصیلی دانشگاهی",
+            dest_entity_title: "رشته های تحصیلی دانشگاهی مقصد",
+            source_entity_code: "609",
+            dest_entity_code: "614",
+            lineage_type: "mdm_2_destination"
+        },
+        {
+            source_entity_title: "زیر داده پایه کسب و کار (مرجع) [Duplicate]",
+            dest_entity_title: "زیر داده پایه کسب و کار",
+            source_entity_code: "604",
+            dest_entity_code: "307",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "دانشگاه ها",
+            dest_entity_title: "دانشگاه های مقصد",
+            source_entity_code: "608",
+            dest_entity_code: "611",
+            lineage_type: "mdm_2_destination"
+        },
+        {
+            source_entity_title: "مدارک تحصیلی مرجع - وزارت علوم",
+            dest_entity_title: "مدارک تحصیلی",
+            source_entity_code: "3051",
+            dest_entity_code: "3001",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "رشته",
+            dest_entity_title: "رشته مقصد",
+            source_entity_code: "606",
+            dest_entity_code: "612",
+            lineage_type: "mdm_2_destination"
+        },
+        {
+            source_entity_title: "مدارک تحصیلی مرجع _  حوزه علمیه [Duplicate]",
+            dest_entity_title: "مدارک تحصیلی",
+            source_entity_code: "3067",
+            dest_entity_code: "3001",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "دانشجو",
+            dest_entity_title: "دانشجو مخزن",
+            source_entity_code: "2002",
+            dest_entity_code: "106",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "دانشگاه های خارجی جدید",
+            dest_entity_title: "دانشگاه خارجی مقصد",
+            source_entity_code: "601",
+            dest_entity_code: "605",
+            lineage_type: "mdm_2_destination"
+        },
+        {
+            source_entity_title: "مدارک تحصیلی مرجع _  حوزه علمیه [Duplicate]",
+            dest_entity_title: "مدارک تحصیلی",
+            source_entity_code: "3068",
+            dest_entity_code: "3001",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "اطلاعات فارغ التحصیلی مرجع",
+            dest_entity_title: "اطلاعات فارغ التحصیلی",
+            source_entity_code: "1000",
+            dest_entity_code: "620",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "دانشگاه خارجی مرجع جدید",
+            dest_entity_title: "دانشگاه های خارجی جدید",
+            source_entity_code: "602",
+            dest_entity_code: "601",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "مدارک تحصیلی مرجع _  حوزه علمیه",
+            dest_entity_title: "مدارک تحصیلی",
+            source_entity_code: "3054",
+            dest_entity_code: "3001",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "اطلاعات پذیرش نهایی",
+            dest_entity_title: "اطلاعات پذیرش نهایی مخزن",
+            source_entity_code: "2003",
+            dest_entity_code: "107",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "پرسنل",
+            dest_entity_title: "پرسنل مخزن",
+            source_entity_code: "2006",
+            dest_entity_code: "109",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "دانشگاه ها",
+            dest_entity_title: "دانشگاه های مقصد [Duplicate]",
+            source_entity_code: "608",
+            dest_entity_code: "615",
+            lineage_type: "mdm_2_destination"
+        },
+        {
+            source_entity_title: "اطلاعات تکمیلی فرد",
+            dest_entity_title: "اطلاعات تکمیلی فرد مخزن",
+            source_entity_code: "2004",
+            dest_entity_code: "108",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "زیر داده پایه کسب و کار (مرجع)",
+            dest_entity_title: "زیر داده پایه کسب و کار",
+            source_entity_code: "400",
+            dest_entity_code: "307",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "علمی کاربردی مرجع",
+            dest_entity_title: "فارغ التحصیل های علمی کاربردی",
+            source_entity_code: "991",
+            dest_entity_code: "990",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "زیر داده پایه کسب و کار تست",
+            dest_entity_title: "زیر داده پایه کسب و کار",
+            source_entity_code: "309",
+            dest_entity_code: "307",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "زیر کاتالوگ (مرجع)",
+            dest_entity_title: "زیر داده پایه کسب وکار",
+            source_entity_code: "402",
+            dest_entity_code: "307",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "دانشگاه های مرجع",
+            dest_entity_title: "دانشگاه ها",
+            source_entity_code: "610",
+            dest_entity_code: "608",
+            lineage_type: "source_to_mdm"
+        },
+        {
+            source_entity_title: "مدارک تحصیلی",
+            dest_entity_title: "مدارک تحصیلی ( مقصد )",
+            source_entity_code: "3001",
+            dest_entity_code: "3065",
+            lineage_type: "mdm_2_destination"
+        },
+        {
+            source_entity_title: "رشته مرجع",
+            dest_entity_title: "رشته",
+            source_entity_code: "607",
+            dest_entity_code: "606",
+            lineage_type: "source_to_mdm"
+        }
+    ]
+    var namespace = joint.shapes;
+    var graph = new joint.dia.Graph;
+    var paper = new joint.dia.Paper({
+        el: document.getElementById('myDiagram'),
+        model: graph,
+        width: "100%",
+        height: "95vh",
+        gridSize: 1,
+        async: true,
+        sorting: joint.dia.Paper.sorting.APPROX,
+        // defaultAnchor: { name: 'modelCenter' },
+        defaultConnectionPoint: { name: 'rectangle' }
+    });
+
+    let firstPos = 0;
+    const createRect = (text, type) => {
+        var rect = new joint.shapes.standard.Rectangle();
+        rect.resize(320, 30);
+        firstPos = firstPos + 35;
+        rect.attr({
+            body: {
+                fill: '#E67E22',
+                stroke: '#D35400'
+            },
+            label: {
+                text: text,
+                fill: 'white'
+            }
+        });
+        if (type == "source") {
+            rect.position(200, firstPos);
+        } else if (type == "mdm") {
+            rect.position(1000, firstPos);
+        } else if (type == "destination") {
+            rect.position(1800, firstPos);
+        }
+        rect.addTo(graph);
+        // rect.toBack();
+        return rect;
+    }
+
+
+
+    const createSourceEntity = () => {
+        let sourceArray = []
+        firstPos = 0
+        data.forEach(function (single) {
+            if (single['lineage_type'] == "source_to_mdm") {
+                if (!sourceArray.includes(single["source_entity_code"])) {
+                    sourceArray.push({
+                        code: single["source_entity_code"],
+                        title: single["source_entity_title"],
+                        pointer: createRect(single["source_entity_title"], "source")
+                    })
+                }
+            }
+        })
+        return sourceArray;
+    }
+
+    const createMdmEntity = () => {
+        let mdmArray = []
+        firstPos = 0
+        data.forEach(function (single) {
+            if (single['lineage_type'] == "source_to_mdm") {
+                if (!mdmArray.includes(single["dest_entity_code"])) {
+                    mdmArray.push({
+                        code: single["dest_entity_code"],
+                        title: single["dest_entity_title"],
+                        pointer: createRect(single["dest_entity_title"], "mdm")
+                    })
+                }
+            }
+        })
+        return mdmArray;
+    }
+
+    const createDestinationEntity = () => {
+        let destinationArray = []
+        firstPos = 0
+        data.forEach(function (single) {
+            if (single['lineage_type'] == "mdm_2_destination") {
+                if (!destinationArray.includes(single["dest_entity_code"])) {
+                    destinationArray.push({
+                        code: single["dest_entity_code"],
+                        title: single["dest_entity_title"],
+                        pointer: createRect(single["dest_entity_title"], "destination")
+                    })
+                }
+            }
+        })
+        return destinationArray;
+    }
+
+
+
+    const createLink = (source, target, text = "", type = "relation") => {
+        var link = new joint.shapes.standard.Link();
+        // link.set('connector', { name: 'normal' });
+        // link.set('connector', { name: 'smooth' });
+        // link.set({ name: 'rounded', args: { radius: 5 } });
+
+        link.source(source, {
+            anchor: {
+                name: 'right',
+            }
+        });
+        link.target(target, {
+            anchor: {
+                name: 'left',
+            }
+        });
+        link.attr({
+            line: {
+                stroke: 'gray',
+                strokeWidth: 2,
+                sourceMarker: {
+                    'type': 'path',
+                    'stroke': 'black',
+                    'fill': 'gray',
+                    'd': 'M 10 -5 0 0 10 5 Z'
+                },
+                targetMarker: {
+                    'type': 'path',
+                    'stroke': 'black',
+                    'fill': 'red',
+                    'd': 'M 10 -5 0 0 10 5 Z'
+                }
+            }
+        });
+        // link.connector('jumpover', { size: 10 });
+        link.labels([{
+            attrs: {
+                text: {
+                    text: text
+                }
+            }
+        }]);
+
+        link.addTo(graph);
+
+    }
+
+    const makeDiagram = () => {
+        let sourceArray = createSourceEntity();
+        let mdmArray = createMdmEntity();
+        let destinationArray = createDestinationEntity();
+
+        data.forEach(function (single) {
+            if (single['lineage_type'] == "source_to_mdm") {
+                let sourceIndex = sourceArray.findIndex((element) => element.code == single['source_entity_code']);
+                let mdmIndex = mdmArray.findIndex((element) => element.code == single['dest_entity_code']);
+                createLink(sourceArray[sourceIndex].pointer, mdmArray[mdmIndex].pointer);
+            } else if (single['lineage_type'] == "mdm_2_destination") {
+                let mdmIndex = mdmArray.findIndex((element) => element.code == single['source_entity_code']);
+                let destinationIndex = destinationArray.findIndex((element) => element.code == single['dest_entity_code']);
+                createLink(mdmArray[mdmIndex].pointer, destinationArray[destinationIndex].pointer);
+            }
+        })
+    }
+
+
+
+    makeDiagram();
+
+})();
--- erd.js
+++ erd.js
@@ -11,11 +11,112 @@
         gridSize: 1,
         async: true,
         frozen: true,
+        cellViewNamespace: joint.shapes,
         sorting: joint.dia.Paper.sorting.APPROX,
         defaultAnchor: { name: 'modelCenter' },
         defaultConnectionPoint: { name: 'anchor' }
     });
 
+
+    // Create a custom element.
+    // ------------------------
+
+    joint.shapes.html = {};
+    joint.shapes.html.Element = joint.shapes.basic.Rect.extend({
+        defaults: joint.util.deepSupplement({
+            type: 'html.Element',
+            attrs: {
+                rect: { stroke: 'none', 'fill-opacity': 0 }
+            }
+        }, joint.shapes.basic.Rect.prototype.defaults)
+    });
+
+    // Create a custom view for that element that displays an HTML div above it.
+    // -------------------------------------------------------------------------
+
+    joint.shapes.html.ElementView = joint.dia.ElementView.extend({
+
+        template: [
+            '<div class="html-element">',
+            // '<button class="delete">x</button>',
+            '<label></label>',
+            '<ul></ul>',
+            '<span></span>', '<br/>',
+            '</div>'
+        ].join(''),
+
+        initialize: function () {
+            _.bindAll(this, 'updateBox');
+            joint.dia.ElementView.prototype.initialize.apply(this, arguments);
+
+            this.$box = $(_.template(this.template)());
+            // Prevent paper from handling pointerdown.
+            this.$box.find('input,select').on('mousedown click', function (evt) {
+                evt.stopPropagation();
+            });
+            // This is an example of reacting on the input change and storing the input data in the cell model.
+            this.$box.find('input').on('change', _.bind(function (evt) {
+                this.model.set('input', $(evt.target).val());
+            }, this));
+            this.$box.find('select').on('change', _.bind(function (evt) {
+                this.model.set('select', $(evt.target).val());
+            }, this));
+            this.$box.find('select').val(this.model.get('select'));
+            this.$box.find('.delete').on('click', _.bind(this.model.remove, this.model));
+            // Update the box position whenever the underlying model changes.
+            this.model.on('change', this.updateBox, this);
+            // Remove the box when the model gets removed from the graph.
+            this.model.on('remove', this.removeBox, this);
+
+            this.updateBox();
+        },
+        render: function () {
+            joint.dia.ElementView.prototype.render.apply(this, arguments);
+            this.paper.$el.prepend(this.$box);
+            this.updateBox();
+            return this;
+        },
+        updateBox: function () {
+            // Set the position and dimension of the box so that it covers the JointJS element.
+            var bbox = this.model.getBBox();
+            // Example of updating the HTML with a data stored in the cell model.
+            this.$box.find('label').text(this.model.get('label'));
+            this.$box.find('ul').text(this.model.get('ul'));
+            this.$box.find('span').text(this.model.get('select'));
+            this.$box.css({
+                width: bbox.width,
+                height: bbox.height,
+                left: bbox.x,
+                top: bbox.y,
+                transform: 'rotate(' + (this.model.get('angle') || 0) + 'deg)'
+            });
+        },
+        removeBox: function (evt) {
+            this.$box.remove();
+        }
+    });
+
+    // Create JointJS elements and add them to the graph as usual.
+    // -----------------------------------------------------------
+
+    var el1 = new joint.shapes.html.Element({
+        position: { x: 80, y: 80 },
+        size: { width: 150, height: 200 },
+        label: 'salam',
+        ul: 'test test2 test3 salam in yek field toolanie',
+        select: 'one'
+    });
+    var el2 = new joint.shapes.html.Element({
+        position: { x: 370, y: 160 },
+        size: { width: 150, height: 200 },
+        label: 'Me_too',
+        select: 'two'
+    });
+    var l = new joint.dia.Link({
+        source: { id: el1.id },
+        target: { id: el2.id },
+        attrs: { '.connection': { 'stroke-width': 5, stroke: '#34495E' } }
+    });
 
     const createLink = (source, target, text = "", type = "relation") => {
         var link = new joint.shapes.standard.Link();
@@ -96,25 +197,60 @@
 
 
     const createEntity = (entityName, attributes = [{}], primaryKey = null) => {
-        const entity = createShape(entityName, 'standard.Rectangle');
-
-        attributes &&
-            attributes.constructor === Array &&
-            attributes.forEach(function (single) {
-                let attribute;
-                if (primaryKey == single) {
-                    attribute = createShape(single, 'standard.Ellipse', 'underline');
-                } else {
-                    attribute = createShape(single, 'standard.Ellipse');
-                }
-                createLink(entity, attribute, "", "attrLink");
-            })
+        var entity = new joint.shapes.html.Element({
+            position: { x: 80, y: 80 },
+            size: { width: 150, height: 180 },
+            label: entityName,
+            ul: attributes.join(' '),
+        });
+        graph.addCells([entity]);
         return entity;
+        // attributes &&
+        //     attributes.constructor === Array &&
+        //     attributes.forEach(function (single) {
+        //         let attribute;
+        //         if (primaryKey == single) {
+        //             attribute = createShape(single, 'standard.Ellipse', 'underline');
+        //         } else {
+        //             attribute = createShape(single, 'standard.Ellipse');
+        //         }
+        //         createLink(entity, attribute, "", "attrLink");
+        //     })
+        // return entity;
     }
 
     const linkEntity = (source, target, text) => {
-        createLink(source, target, text);
+        // createLink(source, target, text);
+        var link = new joint.dia.Link({
+            source: { id: source.id },
+            target: { id: target.id },
+            attrs: {
+                line: {
+                    stroke: '#3498DB',
+                    strokeWidth: 3,
+                    strokeDasharray: '5 5',
+                    strokeDashoffset: 7.5,
+                    sourceMarker: {
+                        'type': 'path',
+                        'stroke': 'none',
+                        'fill': '#3498DB',
+                        'd': 'M 20 -10 0 0 20 10 Z \
+                        M 40 -10 20 0 40 10 Z'
+                    },
+                    targetMarker: {
+                        'type': 'path',
+                        'stroke': 'none',
+                        'fill': '#3498DB',
+                        'd': 'M 7.5 -10 2.5 -10 2.5 10 7.5 10 Z \
+                        M 17.5 -10 12.5 -10 12.5 10 17.5 10 Z \
+                        M 40 -10 20 0 40 10 Z'
+                    }
+                }, '.connection': { 'stroke-width': 2, stroke: '#34495E', }
+            }
+        });
+        graph.addCells([link]);
     }
+
 
     const contact = createEntity("contact", ["id", "user_id", "first_name", "last_name", "phone_number", "join_date", "leave_date"], "id");
     const user = createEntity("user", ["id", "username", "password", "subscription", "email", "expire_date"], "id");
@@ -137,13 +273,15 @@
 
 
 
+
+
     var graphLayout = new joint.layout.ForceDirected({
         graph: graph,
         width: 1500,
         height: 1500,
         gravityCenter: { x: 800, y: 300 },
-        charge: 1300,
-        linkDistance: 10,
+        charge: 10000,
+        linkDistance: 250,
     });
 
     graphLayout.start();
@@ -170,45 +308,4 @@
     }
 
     $('#btn-layout').on('click', startAnimate);
-
-
-
-    // joint.shapes.basic.DecoratedRect = joint.shapes.basic.Generic.extend({
-
-    //     markup: '<g class="rotatable"><g class="scalable"><rect/></g><image/><text/></g>',
-
-    //     defaults: joint.util.deepSupplement({
-
-    //         type: 'basic.DecoratedRect',
-    //         size: { width: 100, height: 60 },
-    //         attrs: {
-    //             'rect': { fill: '#FFFFFF', stroke: 'black', width: 100, height: 60 },
-    //             'text': { 'font-size': 14, text: '', 'ref-x': .5, 'ref-y': .5, ref: 'rect', 'y-alignment': 'middle', 'x-alignment': 'middle', fill: 'black' },
-    //             'text2': { 'font-size': 24, text: 'some text', 'ref-x': .7, 'ref-y': .7, ref: 'rect', 'y-alignment': 'middle', 'x-alignment': 'middle', fill: 'black' },
-
-    //             'image': { 'ref-x': 2, 'ref-y': 2, ref: 'rect', width: 16, height: 16 }
-    //         }
-
-    //     }, joint.shapes.basic.Generic.prototype.defaults)
-    // });
-
-    // var decoratedRect = new joint.shapes.basic.DecoratedRect({
-    //     position: { x: 150, y: 80 },
-    //     size: { width: 100, height: 60 },
-    //     attrs: { 
-    //         text: { text: 'My Element' },
-    //         text2: { text: 'hello world' },
-
-    //         image: { 'xlink:href': 'https://assets-global.website-files.com/63061d4ee85b5a18644f221c/64db4e708003b5f97676b7b1_react.png' }
-    //     }
-    // });
-    // graph.addCell(decoratedRect);
-
-
-
-
-
-
-
-
 })();
